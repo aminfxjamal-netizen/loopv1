f044cc1a1e8dfe6b5ccda1968f909f40e60fb5ed
Jamal Hussein <jamalderow2@gmail.com>
2026-06-01 08:05:02 +0300

    remove wrong supabase location

--- app/pricing/billing/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://wodpovoejpqhqkowheuf.supabase.co'
-const supabaseAnonKey = 'sb_publishable_lvUeA307L2LKDwOtT2YqRA_cqs5Nex2'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
